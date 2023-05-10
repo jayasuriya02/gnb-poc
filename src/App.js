08bed1df2d25cfab4c899eacdf6f1aa2637c5abe
Vishal-HFN <vishal.prakash@volunteer.heartfulness.org>
2023-05-10 14:12:14 +0530

    Added react routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import './App.css';
-import { Button, Container, Navbar } from 'react-bootstrap';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateExam from './pages/CreateExam';
 
 function App() {
-  return <Container fluid>
-    <Navbar expand="lg" variant="light" bg="light">
-      <Container>
-        <Navbar.Brand href="#"><b>Gnb Career</b></Navbar.Brand>
-      </Container>
-    </Navbar>
-    <br></br>
-    <br></br>
-    <br></br>
-    <div className="Button-b">
-      <Button variant="warning">Create Exam</Button>
-      <Button variant="success">Attend Exam</Button>
-      <Button variant="info">Report</Button>
-    </div>
-  </Container>
+  return <BrowserRouter>
+    <Routes>
+
+      <Route exact path='/' Component={Home} />
+      <Route path='/create-exam' Component={CreateExam} />
+    </Routes>
+  </BrowserRouter>
 
 }
 
