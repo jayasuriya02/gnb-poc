22f2346c391b5a81af3afeac7d5b08ea02721ee5
Vishal-HFN <vishal.prakash@volunteer.heartfulness.org>
2023-05-10 13:45:13 +0530

    removed unwanted files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import { Button, Container, Navbar } from 'react-bootstrap';
 
 function App() {
   return <Container fluid>
-     <Navbar expand="lg" variant="light" bg="light">
+    <Navbar expand="lg" variant="light" bg="light">
       <Container>
         <Navbar.Brand href="#"><b>Gnb Career</b></Navbar.Brand>
       </Container>
@@ -13,12 +12,12 @@ function App() {
     <br></br>
     <br></br>
     <div className="Button-b">
-    <Button variant="warning">Create Exam</Button>
-    <Button variant="success">Attend Exam</Button>
-    <Button variant="info">Report</Button>
+      <Button variant="warning">Create Exam</Button>
+      <Button variant="success">Attend Exam</Button>
+      <Button variant="info">Report</Button>
     </div>
   </Container>
-    
+
 }
 
 export default App;
